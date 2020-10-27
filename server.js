772de79e400625130835a31eefa67974a58fb41b
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-10-27 18:53:03 -0400

    Updated Port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const PORT = 3100;
+const PORT = process.env.PORT || 3100;
 
 const app = express();
 
